46db3a5065ecb1326dfeb0dd912b631ab69da62d
Julya <julya.meleshko@gmail.com>
2023-11-01 00:00:54 +0200

    First_Homework

---
+++ src/HW1/task1/User.java
@@ -0,0 +1,66 @@
+package HW1.task1;
+
+public class User {
+
+    private String name;
+    private String surname;
+    private String age;
+    private String Role;  //////Student, Teacher, Child, Adult and etc...
+
+
+    public User(String name, String surname, String age, String role) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        Role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        int ageInt = Integer.parseInt(age);
+        return ageInt;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getRole() {
+        return Role;
+    }
+
+    public void setRole(String role) {
+        Role = role;
+    }
+
+    public void printGreeting(String name, String surname){
+        System.out.println("Hello " + name + " " + surname);
+    }
+    public void printYear(int age){
+        int yearOfBirth = 2023 - age;
+        System.out.println("So, you are " + age +  " years old. That means you are " + yearOfBirth + " year of Birth");
+    }
+
+    public void printRole (String role){
+        System.out.println("Your Role is " + role );
+    }
+
+
+
+
+}
---
+++ src/HW1/task2/Main.java
@@ -0,0 +1,22 @@
+package HW1.task2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] array = new int[10];
+        for(int i =0; i<array.length; i++){
+            array[i] = rand.nextInt(100);
+        }
+        System.out.println(Arrays.toString(array));
+        QuickSort first = new QuickSort(array);
+        first.quickSort(array, 0, array.length-1);
+
+        System.out.println(Arrays.toString(array));
+
+    }
+
+}
+
